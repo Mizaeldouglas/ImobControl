d0722e5825f7e0977228e34ab6868ceed16c36f6
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2025-01-31 15:02:11 -0300

    feat: Create Props in State Class

--- ImobControl/Models/Estado.cs
+++ ImobControl/Models/Estado.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ImobControl.Models;
 
 public class Estado
 {
+    [Key]
+    [StringLength(2, MinimumLength = 2, ErrorMessage = "A Sigla do estado deve ter 2 caracteres")]
+    public string Sigla { get; set; } = string.Empty;
 
+    [Required(ErrorMessage = "O nome do estado é obrigatório")]
+    public string Nome { get; set; } = string.Empty;
 }
